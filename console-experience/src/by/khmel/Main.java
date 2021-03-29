9d63e3ba2e76e26f5bdfbe271a54c59a726b397d
Khmel19 <khmialinka@gmail.com>
2021-03-29 22:46:09 +0300

    added first task: console-experience

---
+++ console-experience/src/by/khmel/Main.java
@@ -0,0 +1,8 @@
+package by.khmel;
+
+public class Main {
+    public static void main(String[] args) {
+        Person smartPerson = new Person("Yura");
+        System.out.println("Hello, " + smartPerson.getName());
+    }
+}
---
+++ console-experience/src/by/khmel/Person.java
@@ -0,0 +1,13 @@
+package by.khmel;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
